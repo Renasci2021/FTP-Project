137586ce506807281850a1527e7432203d2a695c
Rena <lihaoxua21@mails.tsinghua.edu.cn>
2024-09-29 05:32:04 +0800

    feat: add client framework
    - now the client can connect to the server
    - buffer handling in client has bugs

---
+++ client/src/Commands.cs
@@ -0,0 +1,28 @@
+namespace FtpClient;
+
+public class Commands
+{
+    public static void Execute(string command, FtpClient client)
+    {
+        string[] parts = command.Split(' ');
+        string cmd = parts[0].ToUpper();
+
+        switch (cmd)
+        {
+            case "USER":
+                client.ProcessCommand(command);
+                break;
+            case "PASS":
+                client.ProcessCommand(command);
+                break;
+            case "QUIT":
+                client.ProcessCommand(command);
+                client.Disconnect();
+                Environment.Exit(0);
+                break;
+            default:
+                Console.WriteLine("Invalid command");
+                break;
+        }
+    }
+}
---
+++ client/src/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FtpClient;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the FTP client!");
+
+        FtpClient client = new FtpClient();
+        client.Connect("127.0.0.1", 21);
+
+        while (true)
+        {
+            Console.Write("ftp> ");
+            string command = Console.ReadLine();
+            Commands.Execute(command, client);
+        }
+
+        client.Disconnect();
+    }
+}
