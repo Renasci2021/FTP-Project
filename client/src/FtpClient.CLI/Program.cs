13f545958a8d4711ff4087a1ce285265a338e6dc
Rena <lihaoxua21@mails.tsinghua.edu.cn>
2024-10-14 15:24:44 +0800

    refactor: reorganize project structure
    - move client files to client/src
    - move server files to server/src
    - generate a library FtpClient.Core for client
    - generate a console FtpClient.CLI for client

---
+++ client/src/FtpClient.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FtpClient.CLI;
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var massage = FtpClient.Core.Class1.GetMessage();
+        Console.WriteLine(massage);
+    }
+}
---
+++ client/src/FtpClient.Core/FtpClient.cs
@@ -0,0 +1,9 @@
+﻿namespace FtpClient.Core;
+
+public class FtpClient
+{
+    public static string GetMessage()
+    {
+        return "Hello from FtpClient.Core!";
+    }
+}
